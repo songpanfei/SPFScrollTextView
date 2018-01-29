7cf5ae5daac549ba5813a9267b6005f7772fa796
songpanfei <songpanfei@guazi.com>
2018-01-29 19:57:40 +0800

    修改文件问津阿

--- SPFScrollTextView/LMJScrollTextView2.h
+++ SPFScrollTextView/LMJScrollTextView2.h
@@ -11,37 +11,10 @@
 //
 
 #import <UIKit/UIKit.h>
-@class LMJScrollTextView2;
 
-@protocol LMJScrollTextView2Delegate <NSObject>
-
-@optional
-- (void)scrollTextView2:(LMJScrollTextView2 *)scrollTextView currentTextIndex:(NSInteger)index;
-- (void)scrollTextView2:(LMJScrollTextView2 *)scrollTextView clickIndex:(NSInteger)index content:(NSString *)content;
-
-@end
 
 @interface LMJScrollTextView2 : UIView
 
-@property (nonatomic,assign) id <LMJScrollTextView2Delegate>delegate;
 
-/* textDataArr 支持 NSString 和 NSAttributedString 类型 */
-@property (nonatomic,copy)   NSArray * textDataArr;
-
-
-@property (nonatomic,copy)   UIFont  * textFont;
-@property (nonatomic,copy)   UIColor * textColor;
-@property (nonatomic)        NSTextAlignment textAlignment;
-
-@property (nonatomic,assign) BOOL touchEnable; // defualt is YES
-
-- (void)startScrollBottomToTopWithSpace;
-- (void)startScrollTopToBottomWithSpace;
-
-- (void)startScrollBottomToTopWithNoSpace;
-- (void)startScrollTopToBottomWithNoSpace;
-
-- (void)stop;
-- (void)stopToEmpty;
 
 @end
